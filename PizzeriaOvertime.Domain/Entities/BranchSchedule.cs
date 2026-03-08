1a24e95b27e9e50610f9ab8c33b3eac84268fc2c
Andre Vergara <andrevz11@icloud.com>
2026-03-07 20:49:08 -0400

    Create branch aggregate
    Add entities and value objects for branches

---
+++ PizzeriaOvertime.Domain/Entities/BranchSchedule.cs
@@ -0,0 +1,22 @@
+namespace PizzeriaOvertime.Domain.Entities;
+
+public class BranchSchedule
+{
+    public DayOfWeek Day { get; set; }
+    public TimeOnly OpensAt { get; set; }
+    public TimeOnly ClosedAt { get; set; }
+
+    private BranchSchedule(DayOfWeek day, TimeOnly opensAt, TimeOnly closedAt)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(opensAt, closedAt);
+        
+        Day = day;
+        OpensAt = opensAt;
+        ClosedAt = closedAt;
+    }
+
+    public static BranchSchedule Create(DayOfWeek day, TimeOnly opensAt, TimeOnly closedAt)
+    {
+        return new BranchSchedule(day, opensAt, closedAt);
+    }
+}
